7c59c41b63e9c0e282e1a7009431b3725c9b14f7
KevinYakha <kevin@yakha.eu>
2024-04-17 15:39:41 +0200

    implemented basic pattern matching without any flags for single files

--- Grep.cs
+++ Grep.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Xunit.Sdk;
 
 public static class Grep
 {
     public static string Match(string pattern, string flags, string[] files)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string matches = "";
+        foreach (string file in files)
+        {
+            using (StreamReader sr = new StreamReader(files[0]))
+            {
+                for (string line; (line = sr.ReadLine()) != null;)
+                {
+                    if (line.Contains(pattern))
+                        matches += matches == "" ? line : "\n" + line; // only add a newline when it's the first match
+                }
+            }
+        }
+        return matches;
     }
 }
